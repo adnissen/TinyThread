4fc1fcbad8019adcd81cbceab27121b0227c2bc7
Andrew Nissen <andrew_nissen@yahoo.com>
2013-05-22 17:58:58 -0400

    started adding specific thread routing

--- tinythread/client/routes.js
+++ tinythread/client/routes.js
@@ -2,12 +2,18 @@ Meteor.Router.add({
   '/' : 'home',
 
   '/posts/:id': function(id) {
-    console.log('we are at ' + this.canonicalPath);
-    console.log("our parameters: " + this.params);
 
-    // access parameters in order a function args too
-    Session.set('currentPostId', id);
-    return 'threadView';
+  	if (Threads.find({_id: id}).count() != 0)
+  	{
+	    console.log('we are at ' + this.canonicalPath);
+	    console.log("our parameters: " + this.params);
+
+	    // access parameters in order a function args too
+	    Session.set('currentPostId', id);
+	    return 'threadView';
+	}
+	else
+		return 'not_found';
   },
 
   '*': 'not_found'
