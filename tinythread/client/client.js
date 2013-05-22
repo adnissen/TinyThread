ddc923725964ab0b4e7f3d97c47ab5fb15e4a9a8
Andrew Nissen <andrew_nissen@yahoo.com>
2013-05-22 11:43:19 -0400

    took out last of the boilerplate

--- tinythread/client/client.js
+++ tinythread/client/client.js
@@ -2,18 +2,6 @@ Threads = new Meteor.Collection("threads")
 threadsSubscription = Meteor.subscribe("threads");	
 directorySubscription = Meteor.subscribe("directory");
 
-Template.hello.greeting = function () {
-    return "Welcome to tinythread.";
-  };
-
-Template.hello.thread = function() {
+Template.threadView.thread = function() {
 	return Threads.find();
   };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
